394f35124c020f0632c07cf940f3e5e5721c2e9b
Minhkaka <hanoicuatoi1@gmail.com>
2023-04-20 15:37:36 +0700

    2 files modified

--- src/app/dialog-login/dialog-login.component.ts
+++ src/app/dialog-login/dialog-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { LoginAuthService } from '../_services/login-auth.service';
 
@@ -13,22 +14,35 @@ export interface DialogData {
   templateUrl: './dialog-login.component.html',
 })
 export class DialogLoginComponent implements OnInit {
+  signInForm: FormGroup = new FormGroup({});
+
   constructor(
+    private fb: FormBuilder,
     private authdataService: LoginAuthService,
     public dialogRef: MatDialogRef<DialogLoginComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.signInForm = this.fb.group({
+      username: '',
+      password: '',
+      rememberMe: false,
+    });
+  }
 
   submitLogin(): void {
     this.authdataService
-      .authLogin(this.data.username, this.data.password)
+      .authLogin(
+        this.signInForm.get('username').value,
+        this.signInForm.get('password').value
+      )
       .subscribe(
         (data) => {
           if (Object.prototype.hasOwnProperty.call(data['token'], 'error')) {
             // console.log('DialogLoginComponent: login: error', data);
           } else {
+            this.data = this.signInForm.getRawValue();
             this.data['token'] = data['token'];
             this.dialogRef.close({ data: this.data });
           }
